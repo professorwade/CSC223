49788d066112e069751c9543e1d7823403975468
professorwade <121523833+professorwade@users.noreply.github.com>
2026-04-06 08:52:58 -0400

    Week 13 code for MaxHeap implementation

---
+++ src/week13/Program.java
@@ -0,0 +1,22 @@
+package week13;
+
+import java.util.Arrays;
+
+public class Program {
+    public static void main(String[] args) {
+        MaxHeap maxHeap = new MaxHeap();
+        int[] numbers = { 10, 2, 5, 18, 22 };
+
+        // Add all numbers to the heap
+        for (int number : numbers) {
+            maxHeap.insert(number);
+            System.out.printf("   --> array: %s\n", maxHeap.getHeapArrayString());
+        }
+
+        while (maxHeap.getHeapSize() > 0) {
+            int removedValue = maxHeap.remove();
+            System.out.printf("   --> removed %d, array: %s\n", removedValue,
+                    maxHeap.getHeapArrayString());
+        }
+    }
+}
